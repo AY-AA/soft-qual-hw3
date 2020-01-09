67c385440405f5dc2a490d65bd8e235e2815c99c
avishaiyaniv <avishaiy@post.bgu.il>
2020-01-09 17:43:46 +0200

    For Amit

---
+++ src/test/java/SwapMinMaxTopDown.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SwapMinMaxTopDown {
+
+    private static final int MIN = 1;
+    private static final int MAX = 10;
+    private static final int[] ARR1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private static final int[] ARR2 = new int[]{1, 2, 9, 4, 50, 6, 7, 8, 12, 10};
+    //private static final int[] NULL_ARR = null;
+    Program testProgram;
+
+    @Before
+    public void beforeTests() {
+        testProgram = new MinMaxStub();
+    }
+
+    @Test
+    public void testSwapMinMax() {
+        int testRes1 = testProgram.sumMinMax(ARR1);
+        int testRes2 = testProgram.sumMinMax(ARR2);
+        Assert.assertEquals(11, testRes1);
+        Assert.assertEquals(62, testRes2);
+    }
+
+
+    class MinMaxStub extends Program {
+
+        public int minValue(int[] arr) {
+            return MIN;
+        }
+
+        public int maxValue(int[] arr) {
+            return MAX;
+        }
+    }
+}
