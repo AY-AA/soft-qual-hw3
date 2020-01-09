1cf768eb804a6bf69fc3b6ff0b9425db634d08c1
avishaiyaniv <avishaiy@post.bgu.il>
2020-01-09 20:48:11 +0200

    For Amit

--- src/test/java/SumMinMaxTopDown.java
+++ src/test/java/SumMinMaxTopDown.java
@@ -2,10 +2,12 @@ import org.junit.*;
 
 public class SumMinMaxTopDown {
 
-    private static final int MIN = 1;
-    private static final int MAX = 10;
-    private static final int[] ARR1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    private static final int[] ARR2 = new int[]{1, 2, 9, 4, 50, 6, 7, 8, 12, 10};
+    private static final int MIN1 = 1;
+    private static final int MAX1 = 10;
+    private static final int MIN2 = 1;
+    private static final int MAX2 = 50;
+    private static final int[] ARR1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private static final int[] ARR2 = {1, 2, 9, 4, 50, 6, 7, 8, 12, 10};
     //private static final int[] NULL_ARR = null;
     Program testProgram;
 
@@ -19,18 +21,22 @@ public class SumMinMaxTopDown {
         int testRes1 = testProgram.sumMinMax(ARR1);
         int testRes2 = testProgram.sumMinMax(ARR2);
         Assert.assertEquals(11, testRes1);
-        Assert.assertEquals(62, testRes2);
+        Assert.assertEquals(51, testRes2);
     }
 
 
     class MinMaxStub extends Program {
 
         public int minValue(int[] arr) {
-            return MIN;
+            if (arr == ARR1)
+                return MIN1;
+            return MIN2;
         }
 
         public int maxValue(int[] arr) {
-            return MAX;
+            if (arr == ARR1)
+                return MAX1;
+            return MAX2;
         }
     }
 }
